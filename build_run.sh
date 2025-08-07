dee73cad8807c2cd497503984d54d4bd4f611568
Shahab Shokouhi <shokohishahab@gmail.com>
2025-08-07 15:46:19 -0400

    env variable for orb-slam2 path added to cmake

--- build_run.sh
+++ build_run.sh
@@ -2,5 +2,5 @@ colcon build --packages-select orb_slam2 --cmake-clean-cache
 source install/setup.bash
 ros2 launch orb_slam2 orb_slam.launch.py \
     node_name:=orb_slam_node_0 \
-    vocab_file:=/home/shahab/Documents/Codes/ORB_SLAM2/Vocabulary/ORBvoc.txt \
-    settings_file:=/home/shahab/Documents/Codes/ORB_SLAM2/Examples/RGB-D/realsense.yaml
+    vocab_file:=/home/jetson1/ORB_SLAM2/Vocabulary/ORBvoc.txt \
+    settings_file:=/home/jetson1/ORB_SLAM2/Examples/RGB-D/realsense.yaml
